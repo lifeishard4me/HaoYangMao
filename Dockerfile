07d77ebb047d9d57d45dbe1f7373d53fd80305f2
Mr.R <68503791+lifeishard4me@users.noreply.github.com>
2022-08-12 22:58:20 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM kissyouhunter/maiark:latest
-# COPY arkconfig.json /root/
-# # ADD start.sh /start.sh
-# # CMD /start.sh
+COPY arkconfig.json /root/
 EXPOSE 8082
 VOLUME /MaiARK
+RUN sed -e "s/\$ServerUrl/$ServerUrl/g" /root/arkconfig.json \
+    && sed -e "s/\$ServerClientId/$ServerClientId/g" /root/arkconfig.json\
+    && sed -e "s/\$ServerSecret/$ServerSecret/g" /root/arkconfig.json
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sed -e "s/\$ServerUrl/$ServerUrl/g" >/MaiARK/arkconfig.json
-sed -e "s/\$ServerClientId/$ServerClientId/g" >/MaiARK/arkconfig.json
-sed -e "s/\$ServerSecret/$ServerSecret/g" >/MaiARK/arkconfig.json
+sed -e "s/\$ServerUrl/$ServerUrl/g" /MaiARK/arkconfig.json
+sed -e "s/\$ServerClientId/$ServerClientId/g" /MaiARK/arkconfig.json
+sed -e "s/\$ServerSecret/$ServerSecret/g" /MaiARK/arkconfig.json
